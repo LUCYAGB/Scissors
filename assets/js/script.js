3fa623a7547cbb7b38cd915eb88e32624fcdef88
LUCYAGB <lagbenro14@gmail.com>
2022-11-05 13:59:55 +0000

    Add all styles

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Create a function that calls the icons when clicked
+
+function pickPlayerHand(hand) {
+   console.log(hand);
+
+   // Hide the current page
+  let hands = document.querySelector(".hands");
+  hands.style.display = "none";
+
+  //Show the result of the hand the player picked in a new page
+  
+
+}
